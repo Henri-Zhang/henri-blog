a1bc1dd8a2fdb7dfb30816561799323f51f9efbc
heng.zhang <heng.zhang@ximalaya.com>
2019-11-05 17:45:50 +0800

    配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,27 @@
 module.exports = {
   title: 'LeetCode算法题解',
+  head: [['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `/favicon.ico` }]],
   description: 'Just playing around',
-  base: 'blog',
+  base: '/blog/',
   themeConfig: {
-    nav: [{ text: 'Home', link: '/' }],
-    // sidebar: ['/简单/', '/中等/', '/困难/'],
+    nav: [{ text: 'GitHub', link: 'https://github.com/Henri-Zhang' }],
     sidebar: [
+      '/',
+      {
+        title: '简单',
+        collapsable: true,
+        children: []
+      },
       {
         title: '中等',
         collapsable: true,
-        children: ['/中等/']
+        children: ['/中等/535.TinyURL 的加密与解密']
+      },
+      {
+        title: '困难',
+        collapsable: true,
+        children: []
       }
-    ],
-    sidebarDepth: 2
+    ]
   }
 };
--- docs/中等/README.md
+++ docs/中等/README.md
@@ -1,15 +1,3 @@
 ---
 title: 中等
 ---
-
-## 535.TinyURL 的加密与解密
-
-asdfgfdsfgfgb
-
-## ssdd
-
-asdfgfetrbdvffvfvfvvffvfvf
-
-## fgggg
-
-adsfgewgferfgberbgerggrgrgrgrtgrbrtg4rbrtbrtbgejnrkjgjdfnvknfkvfgknbfnbnfnk
