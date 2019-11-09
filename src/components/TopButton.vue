58d9f21566048235b690d4a6083db7968fb8d6c8
heng.zhang <heng.zhang@ximalaya.com>
2019-11-09 14:01:59 +0800

    feat: TopButton组件

--- src/components/TopButton.vue
+++ src/components/TopButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="rounded-circle button" @click="backToTop">
+  <button class="rounded-circle button" :class="animation" @click="backToTop">
     <!-- <FontAwesome name="chevron-up" /> -->
     <span>TOP</span>
   </button>
@@ -8,18 +8,42 @@
 <script>
 export default {
   name: 'TopButton',
-  props: {
-    msg: String,
+  data() {
+    return {
+      animation: '',
+    };
   },
   methods: {
-    backToTop() {
-      console.log('######', '回到顶部');
+    onScroll() {
+      const distance = document.documentElement.scrollTop;
+      if (distance > 200) {
+        this.animation = 'fade-in';
+      } else if (distance <= 200) {
+        this.animation = 'fade-out';
+      }
     },
+    backToTop() {
+      const timer = setInterval(() => {
+        const currentScrollTop = document.documentElement.scrollTop;
+        const distance = Math.ceil(currentScrollTop / 10);
+        document.documentElement.scrollTop = currentScrollTop - distance;
+
+        if (document.documentElement.scrollTop <= 0) {
+          clearInterval(timer);
+        }
+      }, 20);
+    },
+  },
+  created() {
+    window.addEventListener('scroll', this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll);
   },
 };
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .button {
   position: fixed;
   right: 2%;
@@ -28,13 +52,29 @@ export default {
   width: 4rem;
   height: 4rem;
   padding: 10px 0;
-  /* opacity: 0; */
-  /* visibility: hidden; */
+  opacity: 0;
+  visibility: hidden;
   color: white;
   font-weight: bold;
   outline: none;
+  border: none;
   background-color: rgba(0, 0, 0, 0.5);
   cursor: pointer;
   z-index: 3;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  span {
+    line-height: 1.2rem;
+    font-size: 1.2rem;
+    display: block;
+    text-align: center;
+  }
+
+  span:last-child {
+    font-family: Avenir-Light, sans-serif;
+  }
 }
 </style>
