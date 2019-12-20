0fd1f6364ec52693857b497337bc930aa20680ab
heng.zhang <heng.zhang@ximalaya.com>
2019-12-20 18:51:58 +0800

    :bento: 增加代码复制功能

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,14 @@ module.exports = {
   head: [['link', { rel: 'shortcut icon', type: 'image/x-icon', href: `/favicon.ico` }]],
   description: 'Just playing around',
   base: '/blog/',
+  plugins: [
+    [
+      'vuepress-plugin-code-copy',
+      {
+        color: '#f08d49',
+      },
+    ],
+  ],
   themeConfig: {
     nav: [
       { text: 'Home', link: 'https://henri.ren' },
