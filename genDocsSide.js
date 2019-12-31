e50454919e38a1e289dec507340e199a7f4c99e7
heng.zhang <heng.zhang@ximalaya.com>
2019-12-31 19:29:48 +0800

    :rocket: node脚本生成目录

---
+++ genDocsSide.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const dirs = ['简单', '中等', '困难'];
+const sidebar = [];
+
+const genCategory = (dir, files) => {
+  const result = {
+    title: dir,
+    collapsable: true,
+  };
+
+  result.children = files.map(file => `/${dir}/${file.replace('.md', '')}`);
+
+  return result;
+};
+
+dirs.forEach(dir => {
+  const files = fs.readdirSync(`./docs/${dir}`);
+  sidebar.push(genCategory(dir, files));
+});
+
+const config = require('./docs/.vuepress/originConfig.js');
+
+config.themeConfig.sidebar.push(...sidebar);
+
+fs.writeFileSync('./docs/.vuepress/config.js', `module.exports=${JSON.stringify(config)};`);
